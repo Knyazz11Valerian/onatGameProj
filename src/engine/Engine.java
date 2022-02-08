634a4f743a3ddb9e28e717da82a6ef543b4fe14b
Knyazz11Valerian <mega.sklyarov.99@gmail.com>
2022-02-08 13:24:55 +0200

    Created window engine for the game

--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -1,14 +1,34 @@
 package engine;
 
 public class Engine {
+
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 360;
+    public static final String TITLE = "Engine 0.0.1 alpha";
+    private EngineWindow engineWindow;
+
+
     public void run() {
         this.init();
     }
 
     public void init() {
+        this.engineWindow=new EngineWindow(WIDTH,HEIGHT,TITLE);
+        this.engineWindow.create();
+        this.update();
 
     }
-    public void update() {
 
+
+    public void update() {
+        while (!this.engineWindow.isCloseRequest()){
+            this.engineWindow.update();
+            //render pass (рендерится пока не закроешь окно)
+        }
+        this.engineWindow.destroy();
+    }
+
+    public EngineWindow getEngineWindow() {
+        return this.engineWindow;
     }
 }
--- src/engine/EngineWindow.java
+++ src/engine/EngineWindow.java
@@ -3,6 +3,8 @@ package engine;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
 import org.lwjgl.system.MemoryStack;
 
 import java.nio.IntBuffer;
@@ -30,44 +32,57 @@ public class EngineWindow {
     public void create() {
 
         //Проверка на наличие библиотек GLFW
-        if (!GLFW.glfwInit()){
+        if (!GLFW.glfwInit()) {
             System.err.println("GLFW libraries are not initializated.");
             System.exit(-1);
         }
 
         //Создание окна
-        this.id=GLFW.glfwCreateWindow(this.width, this.height, this.title, 0, 0);
+        this.id = GLFW.glfwCreateWindow(this.width, this.height, this.title, 0, 0);
 
         //Закрытие с ошибкой, если создание окна возвращает 0
-        if(this.id==0){
+        if (this.id == 0) {
             System.err.println("There was a problem in creating window.");
             System.exit(-1);
         }
         //Проталкивание кадров
         try (MemoryStack mem = MemoryStack.stackPush()) {
-            this.bufferedWidth= BufferUtils.createIntBuffer(1);
-            this.bufferedHeight= BufferUtils.createIntBuffer(1);
-            GLFW.glfwGetWindowSize(this.id, this.bufferedWidth,this.bufferedHeight);
-        }
-        catch(Exception e){
+            this.bufferedWidth = BufferUtils.createIntBuffer(1);
+            this.bufferedHeight = BufferUtils.createIntBuffer(1);
+            GLFW.glfwGetWindowSize(this.id, this.bufferedWidth, this.bufferedHeight);
+        } catch (Exception e) {
 
         }
+
+        this.videoMode=GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
 
         //Задание параметров окну
-        GLFW.glfwSetWindowTitle(this.id,this.title);
-        GLFW.glfwSetWindowSize(this.id,this.width,this.height);
-        GLFW.glfwSetWindowAspectRatio(this.id,this.width,this.height);
+        GLFW.glfwSetWindowTitle(this.id, this.title);
+        GLFW.glfwSetWindowSize(this.id, this.width, this.height);
+        GLFW.glfwSetWindowAspectRatio(this.id, this.width, this.height);
+        GLFW.glfwSetWindowPos(this.id, ((this.videoMode.width() - this.bufferedWidth.get(0)) / 2),
+                                  (this.videoMode.height() - this.bufferedHeight.get(0)) / 2);
+        GLFW.glfwSetWindowSizeLimits(this.id, this.width, this.height,3840, 2160);
+
+        GLFW.glfwMakeContextCurrent(this.id);
+        GL.createCapabilities();
+        GL11.glViewport(0,0,this.bufferedWidth.get(0),this.bufferedHeight.get(0));
 
     }
 
     //Обновление окна
     public void update() {
-
+        GLFW.glfwPollEvents();
+        GLFW.glfwSwapBuffers(this.id);
     }
 
     //Закрытие окна
     public void destroy() {
+        GLFW.glfwDestroyWindow(this.id);
+    }
 
+    public boolean isCloseRequest(){
+        return GLFW.glfwWindowShouldClose(this.id);
     }
 
     //Геттеры, и сеттеры параметров окна
